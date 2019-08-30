ee3ced48fe457f2cf528e7008f300efa2bc38db1
MrGio7 <lashagiorgi1233@gmail.com>
2019-08-30 18:44:18 +0400

    added background styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route } from "react-router-dom";
 
+import "./Assets/SCSS/App.scss";
+
 import { Navigation } from "./Components";
 
 function App() {
   return (
-    <div>
+    <div className="app">
       <Navigation />
       <Route exact path="/" />
     </div>
