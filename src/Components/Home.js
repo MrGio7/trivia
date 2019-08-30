d825ae21e714ffb1a0f3c2192abf6737ff0a66e3
MrGio7 <lashagiorgi1233@gmail.com>
2019-08-30 19:17:29 +0400

    added home page styling

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import '../Assets/SCSS/Home.scss';
+import "../Assets/SCSS/Home.scss";
 
 const Home = props => {
-    
-    return (
-        <div className="home">
-
-        </div>
-    )
-}
+  return (
+    <div className="home">
+      <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s
+      </p>
+      <input type='button' value="Start Quiz" />
+    </div>
+  );
+};
 
 export default Home;
