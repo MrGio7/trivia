9f457c8bf7f1b9ce0c760e1a4c0ae21d13114fed
MrGio7 <lashagiorgi1233@gmail.com>
2019-08-30 17:29:11 +0400

    added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React from "react";
+import { Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-    <h1>App is online</h1>
+    <div>
+      <Route exact path="/" />
     </div>
   );
 }
