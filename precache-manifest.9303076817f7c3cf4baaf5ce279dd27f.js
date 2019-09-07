d311c55b9b6b574d32966e4cffd1098bbea63283
MrGio7 <lashagiorgi1233@gmail.com>
2019-09-07 20:10:37 +0400

    Updates

---
+++ precache-manifest.9303076817f7c3cf4baaf5ce279dd27f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4771aafe688c1dc3a588c094335831b5",
+    "url": "/trivia/index.html"
+  },
+  {
+    "revision": "b3e03eb70c700c5258fa",
+    "url": "/trivia/static/css/main.7753cb40.chunk.css"
+  },
+  {
+    "revision": "548f8f331ee5b8a19fe9",
+    "url": "/trivia/static/js/2.e1a539fb.chunk.js"
+  },
+  {
+    "revision": "b3e03eb70c700c5258fa",
+    "url": "/trivia/static/js/main.dad44ba3.chunk.js"
+  },
+  {
+    "revision": "e6868d8adeaa46487586",
+    "url": "/trivia/static/js/runtime~main.9e9e68e4.js"
+  }
+]);
