a1606506dd840699a8c7e3324e3fb25fa05afe48
MrGio7 <lashagiorgi1233@gmail.com>
2019-09-07 19:04:13 +0400

    added map data to top

--- src/Components/TopPlayers.js
+++ src/Components/TopPlayers.js
@@ -1,34 +1,41 @@
 import React from "react";
 
-import '../Assets/SCSS/TopPlayers.scss';
+import "../Assets/SCSS/TopPlayers.scss";
 
-const TopPlayers = props =>{
+const TopPlayers = props => {
+  let mapped = props.ranking.map(each => each);
 
-    let mapped = props.ranking.map(each => each)
+  let sorted = mapped.sort((a, b) => b.score - a.score);
 
-    let sorted = mapped.sort((a, b) => b.score - a.score);
+  let reduced = sorted.reduce((acc, cur) => {
+    const x = acc.find(item => item.user === cur.user);
 
-    let reduced = sorted.reduce((acc, cur) => {
-        const x = acc.find(item => item.user === cur.user);
+    if (!x) {
+      return acc.concat([cur]);
+    } else {
+      return acc;
+    }
+  }, []);
 
-        if(!x) {
-            return acc.concat([cur])
-        } else {
-            return acc
-        }
-    }, [])
+  return (
+    <div className="TopPlayers">
+      <div className="header">
+        <h1>Here are our game TOP PLAYERS:</h1>
+      </div>
 
-    return(
-        <div className="TopPlayers">
-            <div className="header">
-                <h1>Here are our game TOP PLAYERS:</h1>
+      <div className="body">
+        {reduced.map((each, index) => {
+          return (
+            <div key={index}>
+              <h2>{index + 1}</h2>
+              <h3>{each.score} </h3>
+              <h4>{each.name} </h4>
             </div>
-
-            <div className="body">
-            <h2>1)</h2>
-            </div>
-        </div>
-    )
-}
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default TopPlayers;
